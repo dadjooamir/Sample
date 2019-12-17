6d251417126e734a2d910a09f4062b5fe1da21fb
Amir Dadjoo <Dadjooamir@gmail.com>
2019-12-17 10:36:13 +0300

    2019-12-17 10:35

---
+++ Sample/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    public class Student
+    {
+        public Student() { }
+        public int StudentID { get; set; }‎
+        public string StudentName { get; set; }‎
+ 
+    }
+
+    public class Statndard
+    {
+        public Standard()‎{}
+         ‎        public int StandardId { get; set; }‎
+         ‎        public string StandardName { get; set; }‎
+         ‎        public string Description { get; set; }‎
+ 
+    }
+}
